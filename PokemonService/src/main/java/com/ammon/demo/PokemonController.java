44beddd85b371c928b9bc5201326f2cea671959a
Ryan J <rjohns91@msn.com>
2019-06-20 10:33:51 -0700

    first commit

---
+++ PokemonService/src/main/java/com/ammon/demo/PokemonController.java
@@ -0,0 +1,22 @@
+package com.ammon.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PokemonController {
+
+	private Pokedex dex;
+	
+	public PokemonController() {
+		dex = new Pokedex();
+	}
+	
+	@GetMapping()
+	public String getPokemon() {		
+		String pokemonName = dex.getPokemon();
+		System.out.println(pokemonName);
+		return pokemonName;
+	}
+	
+}
